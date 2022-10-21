887d3b1c3a30e54500ab2f323adf359925b3c9c6
realerdemdoruk <erdemdoruk3@gmail.com>
2022-10-21 14:59:59 +0300

    Prettier düzeltmesi

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,53 +1,34 @@
-import {useState,useEffect} from 'react';
-
+import { useState, useEffect } from 'react';
 
 function Users() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-const [users, setUsers] = useState([]);
-const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        fetch('https://fakestoreapi.com/products')
-        .then((res) => res.json())
-        .then(data => setUsers(data))
-        .catch(e => console.log(e))
-        .finally(() => setIsLoading (false))
-    },[])
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((e) => console.log(e))
+      .finally(() => setIsLoading(false));
+  }, []);
 
   return (
     <span>
+      <h1>Users</h1>
 
-    <h1>Users</h1>
+      {isLoading && <div> Loading...</div>}
 
+      {users.map((user) => (
+        <div className="card" key={user.id}>
+          <h3>{user.title}</h3>
 
+          <img src={user.image} />
 
-    {isLoading && <div> Loading...</div>}
-
-    {
-
-     
-        users.map((user) => <div className='card' key={user.id}>
-
-        <h3>
-          {user.title}  
-        </h3>
-        
-
-
-        <img src={user.image}/>
-          
-          <p>
-
-          {user.description}</p>
-     
-        
+          <p>{user.description}</p>
         </div>
-        )      
-
-    }
-
+      ))}
     </span>
-  )
+  );
 }
 
-export default Users
+export default Users;
